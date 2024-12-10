7e4a61dd2f19688e9f802a7cb87a87dbd3399b25
Topclass <145531034+BestXigbekele@users.noreply.github.com>
2024-12-10 22:22:56 +0100

    Update wsgi.py

--- pro_list/wsgi.py
+++ pro_list/wsgi.py
@@ -11,6 +11,18 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from django.core.management import call_command
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def create_superuser():
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'password')  # Change as needed
+
+if os.environ.get('RUN_MAIN') == 'true':  # Only run in development mode
+    create_superuser()
+  
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro_list.settings')
 
 application = get_wsgi_application()
