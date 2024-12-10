eb21b9230d28a19fda09d784af3508cc444f1cc0
Topclass <145531034+BestXigbekele@users.noreply.github.com>
2024-12-10 19:50:29 +0100

    Update settings.py

--- pro_list/settings.py
+++ pro_list/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG','False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 
@@ -32,9 +32,6 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 INSTALLED_APPS = [
     'Neoshop',
-    'Blog',
-    'todolist',
-    'da_queen',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
