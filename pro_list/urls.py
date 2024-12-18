a3b8b4e2eb4c51a24efeecf08949750824251e51
Topclass <145531034+BestXigbekele@users.noreply.github.com>
2024-12-18 17:25:37 +0100

    Update urls.py

--- pro_list/urls.py
+++ pro_list/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('Neoshop/',include('Neoshop.urls'))
+     path('oja/',include('Neoshop.urls'))
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
